e4a236995920c2db89336ad982bccb19f423baa3
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-09-18 22:22:48 +0200

    added ex7

--- ex3.c
+++ ex3.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-  int age = 19;
+  int age = 40;
   int height = 180; 
 
   printf("I am %d years old.\n", age);
-  printf("I am %dcm tall.\n", height);
+  printf("I am %d cm tall.\n", height);
 
   return 0;
 }
